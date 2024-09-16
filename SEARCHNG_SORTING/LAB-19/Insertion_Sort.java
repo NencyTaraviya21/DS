690c330f25807a6aeb65cae6df94826b7c948003
NencyTaraviya21 <nencytaraviya2102@gmail.com>
2024-09-16 12:28:49 +0530

    Add files via upload

---
+++ SEARCHNG_SORTING/LAB-19/Insertion_Sort.java
@@ -0,0 +1,21 @@
+
+public class Insertion_Sort {
+    public static void main(String[] args) {
+        
+        int arr[] = new int[]{9,15,14,12,6,8,13};
+        
+       for(int i=0;i<arr.length;i++){
+        int j = i;
+        while((j>0) && (arr[j-1] > arr[j])){
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp; 
+            j--;
+        }
+       }
+
+       for(int k=0;k<arr.length;k++){
+        System.out.print(arr[k]+" ");
+       }
+    }
+}
